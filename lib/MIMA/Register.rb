6a37648c1fba55c58a17376350e80b9e3b7cb668
Master Chief <extra07@online.de>
2012-05-29 23:26:11 +0200

    added bus_read, bus_write, read, write method

--- lib/MIMA/Register.rb
+++ lib/MIMA/Register.rb
@@ -7,6 +7,55 @@ module MIMA
   #
   class Register
 
+    ##
+    # initialize this with a given number of pipes
+    # it can read or write (default 24).
+    # If the buss containes more pipes it onely read an writes 
+    # the last significant pipes.
+    #
+    def initialize num_pipes = 24
+      @bits = Array.new(num_pipes, 0)
+      @read = @write = 0
+    end
+
+    ##
+    # Reads the last significant bits (pipes) from the bus
+    # if this should read
+    #
+    def bus_read pipes
+      if @read == 1
+        for i in (0...@bits.length) do
+          @bits[i] = pipes[i]
+        end
+      end
+    end
+
+    ##
+    # Writes the bits of this to the bus
+    # if this should write
+    #
+    def bus_write
+      if @write == 1
+        @bits.clone
+      else
+        Array.new(@bits.length, 0)
+      end
+    end
+
+    ##
+    # Control pipe to set the read status of this
+    #
+    def read= r
+      @read = (r == 1) ? 1 : 0
+    end
+
+    ##
+    # Control pipe to set the write status of this
+    #
+    def write= w
+      @read = (r == 1) ? 1 : 0
+    end
+
   end
 
 end
