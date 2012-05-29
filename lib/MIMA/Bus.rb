d42865c03167df411cc662c9eacdb63326f38f6d
Master Chief <extra07@online.de>
2012-05-29 21:51:30 +0200

    started working on

---
+++ lib/MIMA/Bus.rb
@@ -0,0 +1,17 @@
+##
+# This class represents a Bus in The MIMA.
+# A Bus has 24 Lines and a set of Components attatched to it.
+# The Buss Reads an Wfrom each Komponent and Then Writes its
+# Status to each Component. So its on the components Site to 
+# Write or Read from the Bus.
+#
+class Buss
+  
+  ##
+  # Validate and Adds a new coponent to this
+  #
+  def add component
+
+  end
+
+end
