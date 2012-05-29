4bb4f280403b47e46242294ee12d852ac816706c
Master Chief <extra07@online.de>
2012-05-29 23:06:42 +0200

    started working on

---
+++ lib/MIMA/Register.rb
@@ -0,0 +1,12 @@
+module MIMA
+  
+  ##
+  # A Register is a MIMA bus component.
+  # A Register can read and save the current state of
+  # the bus pipes and also write its current state to the bus.
+  #
+  class Register
+
+  end
+
+end
