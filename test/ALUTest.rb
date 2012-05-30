57633543c72d7bae36332e72bde26536e99a92fa
Master Chief <extra07@online.de>
2012-05-31 00:28:07 +0200

    ALU x + y -> z tested

--- test/ALUTest.rb
+++ test/ALUTest.rb
@@ -1,4 +1,5 @@
 require './lib/MIMA.rb'
+require './lib/MIMA/Fixnum.rb'
 require 'test/unit'
 
 class ALUTest < Test::Unit::TestCase
@@ -7,26 +8,32 @@ class ALUTest < Test::Unit::TestCase
   
   def test_add
     alu = ALU.new
+    alu.c0 = 1
+
     x = alu.x
     y = alu.y
     z = alu.z
 
-    x.read = 1
-    y.read = 1
+    100.times do
+      x.read = 1
+      y.read = 1
 
-    x.bus_read [1,0,1]
-    y.bus_read [1,1]
+      x.bus_read Array.new(24, 0)
+      y.bus_read Array.new(24, 0)
 
-    alu.c0 = 1
+      x_val =  Random.rand(2**23)
+      y_val =  Random.rand(2**23)
+      z_val =  (x_val + y_val).to_bin_ary
+      z_val << 0 until z_val.length == 24
+ 
+      x.bus_read x_val.to_bin_ary
+      y.bus_read y_val.to_bin_ary
 
-    puts alu.inspect
-    puts x.inspect
-    puts y.inspect
-    puts alu.clk.inspect
+      alu.clk
+      z.write = 1
 
-    z.write = 1
-
-    assert_equal [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0], z.bus_write
+      assert_equal z_val, z.bus_write
+    end
 
   end
 
