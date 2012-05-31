f9bacede1dd3adbb3df91fb3538e57a88d2bf53d
Master Chief <extra07@online.de>
2012-05-31 12:51:08 +0200

    added set_alu

--- lib/MIMA/MicroProrammParser.rb
+++ lib/MIMA/MicroProrammParser.rb
@@ -11,13 +11,13 @@ module MIMA
     #
     # <R1> -> <R2>;     # R1 writes, R2 reads
     # <CP> = <1/0>;     # Sets the Control Pipe CP to 1 / 0
-    # ALU ADD           # lets the ALU calculate X + Y -> Z
-    # ALU rotate        # lets the ALU calculate rotate x right -> Z
-    # ALU AND           # lets the ALU calculate X AND Y -> Z
-    # ALU OR            # lets the ALU calculate X OR Y -> Z
-    # ALU XOR           # lets the ALU calculate X XOR Y -> Z
-    # ALU NOT           # lets the ALU calculate NOT X -> Z
-    # ALU EQL           # lets the ALU calculate if X == Y, -1 -> Z else 0 -> Z
+    # ALU ADD;          # lets the ALU calculate X + Y -> Z
+    # ALU rotate;       # lets the ALU calculate rotate x right -> Z
+    # ALU AND;          # lets the ALU calculate X AND Y -> Z
+    # ALU OR;           # lets the ALU calculate X OR Y -> Z
+    # ALU XOR;          # lets the ALU calculate X XOR Y -> Z
+    # ALU NOT;          # lets the ALU calculate NOT X -> Z
+    # ALU EQL;          # lets the ALU calculate if X == Y, -1 -> Z else 0 -> Z
     # ADR 0x<Address>;  # Sets the address of the folloing Micro-Programm-Comand
     #
     # The Following bits in a Microprogramm Code can be setted trought the discussed Comands:
@@ -57,14 +57,21 @@ module MIMA
     # and each Micro-Programm-Comand must end with an Semicolon
     #
     class MicroProgrammParser
+
+      ##
+      # implementaition of an Parse error
+      #
+      class MicroCodeParseError < RuntimeError
+        def initialize str
+          super("Error while parsing! " + str)
+        end
+      end
       
       ##
       # parses a given Micro-Programm-Code and returns the bit Array for the ControlUnit
       #
       def self.parse str
-
-      def initialize str
-        @bits = Array.new 28, 0
+        bits = Array.new 28, 0
 
         str.split(";").each do |comand|
           args = comand.split(" ")
@@ -75,9 +82,34 @@ module MIMA
             else
           end
         end
-
       end
 
+      ##
+      # sets the ALU control bits in the given bits Array 
+      # depending on the given ALU operation:
+      # 
+      # Operation    | C2 C1 C0
+      # -----------------------
+      # ALU ADD;     |  0  0  1 
+      # ALU rotate;  |  0  1  0
+      # ALU AND;     |  0  1  1
+      # ALU OR;      |  1  0  0
+      # ALU XOR;     |  1  0  1
+      # ALU NOT;     |  1  1  0
+      # ALU EQL;     |  1  1  1
+      #
+      def set_alu bits, op
+        case op
+          when "ADD"    then bits[12,3] = [1,0,0]
+          when "rotate" then bits[12,3] = [0,1,0] 
+          when "AND"    then bits[12,3] = [1,1,0]
+          when "OR"     then bits[12,3] = [0,0,1]
+          when "XOR"    then bits[12,3] = [1,0,1] 
+          when "NOT"    then bits[12,3] = [0,1,1] 
+          when "EQL"    then bits[12,3] = [1,1,1] 
+          else raise MicroProgrammParser.new("ALU unknowen operation: #{ op }")
+        end
+      end
 
     end
 
