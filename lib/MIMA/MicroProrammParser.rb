4d45d339927276e5cc3eead338e46481c0ae0619
Master Chief <extra07@online.de>
2012-05-31 11:36:21 +0200

    startet working on

---
+++ lib/MIMA/MicroProrammParser.rb
@@ -0,0 +1,17 @@
+module MIMA
+  
+  class ControlUnit
+
+    ##
+    # this class implements an parser for the MicroProgramms of the
+    # ControlUnit, it parsers Human readable MicroProgrammCode int
+    # Controll signals for the ControlUnit
+    #
+    class MicroProgrammParser
+
+
+    end
+
+  end
+
+end
