abf82a83a11d68573d304319d5fd2144759c3273
Master Chief <extra07@online.de>
2012-06-01 19:17:23 +0200

    added extensions

--- lib/MIMA.rb
+++ lib/MIMA.rb
@@ -1,9 +1,12 @@
+load './lib/MIMA/String.rb'
+load './lib/MIMA/Array.rb'
+load './lib/MIMA/Fixnum.rb'
 load './lib/MIMA/Bus.rb'
 load './lib/MIMA/Register.rb'
 load './lib/MIMA/Constant.rb'
 load './lib/MIMA/Memory.rb'
 load './lib/MIMA/ALU.rb'
-load './lib/MIMA/MicroProgrammParser.rb'
+load './lib/MIMA/MicroCommand.rb'
 
 module MIMA
   VERSION = '0.0.1'
