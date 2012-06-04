137609dfedf12afd2daa9618c9368b3597719bc1
Master Chief <extra07@online.de>
2012-06-04 11:39:48 +0200

    finisehd initialize

--- lib/MIMA/Mima.rb
+++ lib/MIMA/Mima.rb
@@ -12,8 +12,22 @@ module MIMA
     #
     def initialize num_pipes = 24
       @controlunit = MIMA::ControlUnit.new num_pipes
+      @akku        = @controlunit.akku
+      @iar         = @controlunit.iar
+      @one         = @controlunit.one
+      @ir          = @controlunit.ir
+      @alu         = @controlunit.alu
+      @x           = @controlunit.x
+      @y           = @controlunit.y
+      @z           = @controlunit.z
+      @memory      = @controlunit.memory
+      @mar         = @controlunit.mar
+      @mdr         = @controlunit.mdr
+      @bus         = MIMA::Bus.new num_pipes
+      @register    = [@akku, @iar, @one, @ir, @x, @y, @z, @mar, @mdr]
 
-      
+      # initialize bus
+      @register.each { |r| @bus.add r }
     end
 
   end
