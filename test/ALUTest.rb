2af5d4b4642f0f54593314e924db91c057752d31
Master Chief <extra07@online.de>
2012-06-05 11:30:32 +0200

    added and test

--- test/ALUTest.rb
+++ test/ALUTest.rb
@@ -1,5 +1,4 @@
 require './lib/MIMA.rb'
-require './lib/MIMA/Fixnum.rb'
 require 'test/unit'
 
 class ALUTest < Test::Unit::TestCase
@@ -72,6 +71,44 @@ class ALUTest < Test::Unit::TestCase
       x_val =  Random.rand(2**23)
       y_val =  Random.rand(2**23)
       z_val =  (x_val + y_val).to_bin_ary
+      z_val << 0 until z_val.length == 24
+ 
+      x.bus_read x_val.to_bin_ary
+      y.bus_read y_val.to_bin_ary
+
+      alu.clk
+      z.write = 1
+      x.write = 1
+      y.write = 1
+
+      assert_equal z_val, z.bus_write
+      assert_equal x_val, x.bus_write.bin_to_dez
+      assert_equal y_val, y.bus_write.bin_to_dez
+    end
+
+  end
+
+  def test_and
+    alu = ALU.new
+    alu.c0 = 1
+    alu.c1 = 1
+
+    x = alu.x
+    y = alu.y
+    z = alu.z
+
+    1000.times do
+      x.read = 1
+      y.read = 1
+      x.write = 0
+      y.write = 0
+
+      x.bus_read Array.new(24, 0)
+      y.bus_read Array.new(24, 0)
+
+      x_val =  Random.rand(2**24)
+      y_val =  Random.rand(2**24)
+      z_val =  (x_val & y_val).to_bin_ary
       z_val << 0 until z_val.length == 24
  
       x.bus_read x_val.to_bin_ary
