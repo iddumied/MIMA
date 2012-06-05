d9b88b6e8c104fa82e8a3248f5b74831c13a6b71
Master Chief <extra07@online.de>
2012-06-05 16:33:20 +0200

    added test for long and short commands

--- test/MimaCommandTest.rb
+++ test/MimaCommandTest.rb
@@ -5,6 +5,27 @@ class MicroCommandTest < Test::Unit::TestCase
   
   include MIMA
   
+  def test_short_commands
+    1000.times do
+      comand = MimaCommand::SCOMMANDS.values[Random.rand(MimaCommand::SCOMMANDS.length)]
+      next if comand == "LC"
+
+      addr = []
+      addr << Random.rand(2) until addr.length == 20
+      comand = "#{ comand } #{ addr.bin_to_hex }"
+
+      assert_equal comand, MimaCommand.new(MimaCommand.new(comand).bits).description
+    end
+  end
+
+  def test_long_commands
+    1000.times do
+      comand = MimaCommand::LCOMMANDS.values[Random.rand(MimaCommand::LCOMMANDS.length)]
+
+      assert_equal comand, MimaCommand.new(MimaCommand.new(comand).bits).description
+    end
+  end
+
   def test_alu_comands
     mima = Mima.new
 
@@ -21,6 +42,8 @@ class MicroCommandTest < Test::Unit::TestCase
 
       mima.run "LDC #{ y.bin_to_hex }"
       mima.run "AND #{ addr.bin_to_hex }"
+      puts "[DEBUG] #{ y.bin_to_dez } | #{ x.bin_to_dez } = #{ y.bin_to_dez | x.bin_to_dez }"
+      puts "[DEBUG] #{ y.bin_to_dez } & #{ x.bin_to_dez } = #{ y.bin_to_dez & x.bin_to_dez }"
       assert_equal mima.akku.content.bin_to_dez, (x.bin_to_dez & y.bin_to_dez) 
 
       mima.run "LDC #{ y.bin_to_hex }"
