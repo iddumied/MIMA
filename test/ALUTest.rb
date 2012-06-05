e8802546d076d3865635c747afa0d135097e1268
Master Chief <extra07@online.de>
2012-06-05 11:44:02 +0200

    added eql test

--- test/ALUTest.rb
+++ test/ALUTest.rb
@@ -308,4 +308,74 @@ class ALUTest < Test::Unit::TestCase
 
   end
 
+  def test_eql
+    alu = ALU.new
+    alu.c0 = 1
+    alu.c1 = 1
+    alu.c2 = 1
+
+    x = alu.x
+    y = alu.y
+    z = alu.z
+
+    1000.times do
+      x.read = 1
+      y.read = 1
+      x.write = 0
+      y.write = 0
+
+      x.bus_read Array.new(24, 0)
+      y.bus_read Array.new(24, 0)
+
+      x_val =  Random.rand(2**24)
+      y_val =  Random.rand(2**24)
+      z_val =  (x_val == y_val) ? [1]*24 : [0]*24
+ 
+      x.bus_read x_val.to_bin_ary
+      y.bus_read y_val.to_bin_ary
+
+      alu.clk
+      x.read = 0
+      y.read = 0
+      z.read = 0
+      z.write = 1
+      x.write = 1
+      y.write = 1
+
+      assert_equal z_val, z.bus_write
+      assert_equal x_val, x.bus_write.bin_to_dez
+      assert_equal y_val, y.bus_write.bin_to_dez
+    end
+
+    1000.times do
+      x.read = 1
+      y.read = 1
+      x.write = 0
+      y.write = 0
+
+      x.bus_read Array.new(24, 0)
+      y.bus_read Array.new(24, 0)
+
+      x_val =  Random.rand(2**24)
+      y_val =  x_val
+      z_val =  (x_val == y_val) ? [1]*24 : [0]*24
+ 
+      x.bus_read x_val.to_bin_ary
+      y.bus_read y_val.to_bin_ary
+
+      alu.clk
+      x.read = 0
+      y.read = 0
+      z.read = 0
+      z.write = 1
+      x.write = 1
+      y.write = 1
+
+      assert_equal z_val, z.bus_write
+      assert_equal x_val, x.bus_write.bin_to_dez
+      assert_equal y_val, y.bus_write.bin_to_dez
+    end
+
+  end
+
 end
